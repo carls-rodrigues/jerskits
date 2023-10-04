430ebe365d5ce5ebe9d669a47249b020ed00df99
Carlos Rodrigues <cerf@furg.br>
2023-10-03 23:58:42 -0300

    feat: add Header at Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,13 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import {Inter, Urbanist} from 'next/font/google'
+import Header from "@/app/layout/header/header";
 
 const inter = Inter({ subsets: ['latin'] })
+const urbanist = Urbanist({
+    subsets: ['latin'],
+    weight: ["400", "500", "600", "700"]
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={urbanist.className}>
+      <Header/>
+      {children}
+      </body>
     </html>
   )
 }
