80eeed880356f3bd32e00bbe6af254dfd64c60d9
Carlos Rodrigues <cerf@furg.br>
2023-10-03 23:55:28 -0300

    feat: create Container component

---
+++ src/app/components/container/container.tsx
@@ -0,0 +1,13 @@
+import {createElement, HTMLAttributes, ReactNode} from "react";
+
+type ContainerProps = {
+    children: ReactNode
+} & HTMLAttributes<HTMLElement>
+
+export default function Container({ children, ...props }: ContainerProps) {
+    return (
+        <main {...props}>
+            {children}
+        </main>
+    )
+}
