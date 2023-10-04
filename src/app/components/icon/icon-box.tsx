cbbcc819bdd0d26364a76165231eb35329ceac7d
Carlos Rodrigues <cerf@furg.br>
2023-10-03 23:55:51 -0300

    feat: create IconBox component to handle svg icons

---
+++ src/app/components/icon/icon-box.tsx
@@ -0,0 +1,13 @@
+import {createElement, HTMLAttributes, ReactNode} from "react";
+import Box from "@/app/components/box/box";
+
+type IconBoxProps = {
+    children: JSX.Element
+} & HTMLAttributes<HTMLElement>
+export default function IconBox({ children, ...props }: IconBoxProps) {
+    return (
+        <Box className="flex flex-wrap justify-center w-24 h-24" {...props}>
+            {children}
+        </Box>
+    )
+}
