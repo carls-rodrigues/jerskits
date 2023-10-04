2bb169e09fd2553f5c192de40d7a8e58da253fcb
Carlos Rodrigues <cerf@furg.br>
2023-10-03 23:58:24 -0300

    feat: add Logo component

---
+++ src/app/components/logo/logo.tsx
@@ -0,0 +1,11 @@
+import Box from "@/app/components/box/box";
+
+import LogoIcon from '@/app/assets/icons/logo.svg'
+
+export default function Logo() {
+    return (
+        <Box>
+            <LogoIcon />
+        </Box>
+    )
+}
