00d7d011a7f663f39d09b43e8b9d0c9d3d20e800
Carlos Rodrigues <cerf@furg.br>
2023-10-03 23:59:53 -0300

    feat: add Home to Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Image from 'next/image'
+import Home from "@/app/home";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold text-center">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
-    </main>
-  )
+export default function App() {
+  return <Home />
 }
